32bb76178d6704cb79e37b51d68cef37941f8a66
cyblogerz <pranavajay2002@gmail.com>
2021-10-19 22:19:16 +0530

    Created story model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,11 +1,15 @@
+import 'package:whatsapp_clone/models/story_model.dart';
+
 class User {
   String name;
   String message;
   String time;
+  final List<StoryModel>? stories;
   String pfpurl;
   String about;
   User(
       {required this.name,
+      this.stories,
       required this.message,
       required this.time,
       required this.about,
